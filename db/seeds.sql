ae7947eac353ecc278b26782a71d8ad3e5c1eda9
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-09-26 21:28:51 -0500

    initial commit plus ultra

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('cheese');
+INSERT INTO burgers (burger_name) VALUES ('steak');
+INSERT INTO burgers (burger_name, devoured) VALUES ('pepper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bbq', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon', true);
+INSERT INTO burgers (burger_name) VALUES ('pizza');
